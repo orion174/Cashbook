62bbe729e893794b2725ef5ca64bf34d8a39437e
박범진 <@DESKTOP-LDR8D7J>
2022-04-15 17:27:12 +0900

    [박범진] DButil

--- cashbook/src/main/java/util/DButil.java
+++ cashbook/src/main/java/util/DButil.java
@@ -3,7 +3,7 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+// .
 public class DButil {
 	// DB연결 메서드
 	public static Connection getConnection() {
